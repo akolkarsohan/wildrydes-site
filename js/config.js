5007efa7657eef8d2caa84239088ad09fe16f287
EC2 Default User <ec2-user@ip-172-31-8-95.ec2.internal>
2020-07-23 02:50:35 +0000

    wildrydes test site

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_WG7qPBcgW', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5f8o8ln6vlpe7feffsn3fjcahi', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
